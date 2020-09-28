d9dfc51adb9a66ad43d83b4d9b19702e4e22fa4b
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-09-28 21:18:15 +1000

    Update AnvilInventoryTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/inventory/AnvilInventoryTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/inventory/AnvilInventoryTranslator.java
@@ -36,6 +36,8 @@ import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.geysermc.connector.inventory.Inventory;
 import org.geysermc.connector.network.session.GeyserSession;
+import org.geysermc.connector.network.translators.inventory.action.Transaction;
+import org.geysermc.connector.network.translators.inventory.action.Execute;
 import org.geysermc.connector.network.translators.inventory.updater.CursorInventoryUpdater;
 
 import java.util.List;
@@ -78,61 +80,8 @@ public class AnvilInventoryTranslator extends BlockInventoryTranslator {
     }
 
     @Override
-    public SlotType getSlotType(int javaSlot) {
-        if (javaSlot == 2)
-            return SlotType.OUTPUT;
-        return SlotType.NORMAL;
-    }
-
-    @Override
-    public void translateActions(GeyserSession session, Inventory inventory, List<InventoryActionData> actions) {
-        InventoryActionData anvilResult = null;
-        InventoryActionData anvilInput = null;
-        for (InventoryActionData action : actions) {
-            if (action.getSource().getContainerId() == ContainerId.ANVIL_MATERIAL) {
-                //useless packet
-                return;
-            } else if (action.getSource().getContainerId() == ContainerId.ANVIL_RESULT) {
-                anvilResult = action;
-            } else if (bedrockSlotToJava(action) == 0) {
-                anvilInput = action;
-            }
-        }
-        ItemData itemName = null;
-        if (anvilResult != null) {
-            itemName = anvilResult.getFromItem();
-        } else if (anvilInput != null) {
-            itemName = anvilInput.getToItem();
-        }
-        if (itemName != null) {
-            String rename;
-            NbtMap tag = itemName.getTag();
-            if (tag != null && tag.containsKey("display")) {
-                String name = tag.getCompound("display").getString("Name");
-                try {
-                    Component component = GsonComponentSerializer.gson().deserialize(name);
-                    rename = LegacyComponentSerializer.legacySection().serialize(component);
-                } catch (JsonSyntaxException e) {
-                    rename = name;
-                }
-            } else {
-                rename = "";
-            }
-            ClientRenameItemPacket renameItemPacket = new ClientRenameItemPacket(rename);
-            session.sendDownstreamPacket(renameItemPacket);
-        }
-        if (anvilResult != null) {
-            //Strip unnecessary actions
-            List<InventoryActionData> strippedActions = actions.stream()
-                    .filter(action -> action.getSource().getContainerId() == ContainerId.ANVIL_RESULT
-                            || (action.getSource().getType() == InventorySource.Type.CONTAINER
-                            && !(action.getSource().getContainerId() == ContainerId.UI && action.getSlot() != 0)))
-                    .collect(Collectors.toList());
-            super.translateActions(session, inventory, strippedActions);
-            return;
-        }
-
-        super.translateActions(session, inventory, actions);
+    public boolean isOutput(InventoryActionData action) {
+        return action.getSource().getContainerId() == ContainerId.ANVIL_RESULT;
     }
 
     @Override
@@ -163,5 +112,47 @@ public class AnvilInventoryTranslator extends BlockInventoryTranslator {
             }
         }
         super.updateSlot(session, inventory, slot);
+    }
+
+    @Override
+    public void translateActions(GeyserSession session, Inventory inventory, List<InventoryActionData> actions) {
+        // Fix up Results
+        if (actions.stream().anyMatch(a -> a.getSource().getContainerId() == ContainerId.ANVIL_RESULT)) {
+            actions = actions.stream()
+                    .filter(a -> {
+                        return (a.getSource().getContainerId() != ContainerId.UI
+                                && a.getSource().getContainerId() != ContainerId.CONTAINER_INPUT);
+                    })
+                    .collect(Collectors.toList());
+        }
+
+        super.translateActions(session, inventory, actions);
+    }
+
+    @Override
+    protected void processAction(Transaction transaction, ActionData cursor, ActionData from, ActionData to) {
+        // If from is the output we add a rename packet
+        if (isOutput(from.action)) {
+            transaction.add(new Execute(() -> {
+                String rename;
+                ItemData item = from.action.getFromItem();
+                NbtMap tag = item.getTag();
+                if (tag != null) {
+                    String name = tag.getCompound("display").getString("Name");
+                    try {
+                        Component component = GsonComponentSerializer.gson().deserialize(name);
+                        rename = LegacyComponentSerializer.legacySection().serialize(component);
+                    } catch (JsonSyntaxException e) {
+                        rename = name;
+                    }
+                } else {
+                    rename = "";
+                }
+                ClientRenameItemPacket renameItemPacket = new ClientRenameItemPacket(rename);
+                transaction.getSession().sendDownstreamPacket(renameItemPacket);
+            }));
+        }
+
+        super.processAction(transaction, cursor, from, to);
     }
 }
